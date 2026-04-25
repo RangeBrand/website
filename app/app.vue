bc3226bf7fede575290f4652e9f3830ef190a6d3
Javid Izadfar <javid.izadfar@gmail.com>
2026-04-25 12:56:46 +0330

    feat: customized scrollbar

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+useHead({
+  htmlAttrs: {
+    class:
+      "scrollbar-thin scrollbar-thumb-rounded-full scrollbar-thumb-gray-400 scrollbar-track-rb-violet-100",
+  },
+});
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
